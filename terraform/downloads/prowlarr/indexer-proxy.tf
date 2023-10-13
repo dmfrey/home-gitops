3ae378faf9e9761d38f2eb141c698037ccc449ca
Daniel Frey <dmfrey@gmail.com>
2023-10-13 13:23:02 -0400

    update terraform objects

--- terraform/downloads/prowlarr/indexer-proxy.tf
+++ terraform/downloads/prowlarr/indexer-proxy.tf
@@ -6,5 +6,4 @@ resource "prowlarr_indexer_proxy_flaresolverr" "flaresolverr" {
   host            = "http://flaresolverr.download.svc.cluster.local:8191"
   name            = "Flaresolverr"
   request_timeout = 60
-  tags            = [prowlarr_tag.flaresolverr.id]
 }
