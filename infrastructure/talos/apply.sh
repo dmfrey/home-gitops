fe39fbf616573280875d0bbaf21f292d50c856e2
Daniel Frey <dmfrey@gmail.com>
2023-10-19 09:10:23 -0400

    update config for talhelper

--- infrastructure/talos/apply.sh
+++ infrastructure/talos/apply.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 192.168.30.6 -f ./clusterconfig/homelab-k8s-001-dmf-nuc-001.frey.dev.yaml
+# talosctl apply-config -n 192.168.30.5 -f ./clusterconfig/homelab-k8s-001-dmf-amd-001.frey.dev.yaml
+# talosctl apply-config -n 192.168.30.6 -f ./clusterconfig/homelab-k8s-001-dmf-nuc-001.frey.dev.yaml
 talosctl apply-config -n 192.168.30.10 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-001.frey.dev.yaml
-# talosctl apply-config -n 192.168.30.11 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-002.frey.dev.yaml
-# talosctl apply-config -n 192.168.30.12 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-003.frey.dev.yaml
+talosctl apply-config -n 192.168.30.11 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-002.frey.dev.yaml
+talosctl apply-config -n 192.168.30.12 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-003.frey.dev.yaml
 # talosctl apply-config -n 192.168.30.13 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-004.frey.dev.yaml
