da7016b1fb537e1f24a59962e42fafbf158cbbaf
Daniel Frey <dmfrey@gmail.com>
2023-10-23 10:27:29 -0400

    update talos

--- infrastructure/talos/apply.sh
+++ infrastructure/talos/apply.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-# talosctl apply-config -n 192.168.30.5 -f ./clusterconfig/homelab-k8s-001-dmf-amd-001.frey.dev.yaml
-# talosctl apply-config -n 192.168.30.6 -f ./clusterconfig/homelab-k8s-001-dmf-nuc-001.frey.dev.yaml
 talosctl apply-config -n 192.168.30.21 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-001.frey.dev.yaml
 talosctl apply-config -n 192.168.30.22 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-002.frey.dev.yaml
 talosctl apply-config -n 192.168.30.23 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-003.frey.dev.yaml
-# talosctl apply-config -n 192.168.30.13 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-004.frey.dev.yaml
+
+# talosctl apply-config -n 192.168.30.24 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-004.frey.dev.yaml
+talosctl apply-config -n 192.168.30.31 -f ./clusterconfig/homelab-k8s-001-dmf-amd-001.frey.dev.yaml
+talosctl apply-config -n 192.168.30.41 -f ./clusterconfig/homelab-k8s-001-dmf-nuc-001.frey.dev.yaml
--- infrastructure/talos/bootstrap.sh
+++ infrastructure/talos/bootstrap.sh
@@ -6,9 +6,9 @@ talosctl apply-config --insecure -n 192.168.30.21 -f ./clusterconfig/homelab-k8s
 talosctl apply-config --insecure -n 192.168.30.22 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-002.frey.dev.yaml
 talosctl apply-config --insecure -n 192.168.30.23 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-003.frey.dev.yaml
 
-# talosctl apply-config --insecure -n 192.168.30.5 -f ./clusterconfig/homelab-k8s-001-dmf-amd-001.frey.dev.yaml
-# talosctl apply-config --insecure -n 192.168.30.6 -f ./clusterconfig/homelab-k8s-001-dmf-nuc-001.frey.dev.yaml
-# talosctl apply-config --insecure -n 192.168.30.13 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-004.frey.dev.yaml
+# talosctl apply-config --insecure -n 192.168.30.24 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-004.frey.dev.yaml
+talosctl apply-config --insecure -n 192.168.30.31 -f ./clusterconfig/homelab-k8s-001-dmf-amd-001.frey.dev.yaml
+talosctl apply-config --insecure -n 192.168.30.41 -f ./clusterconfig/homelab-k8s-001-dmf-nuc-001.frey.dev.yaml
 
 echo "Sleeping..."
 sleep 120
