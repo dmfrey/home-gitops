20c3fb392b0d5f3d646bd7510cf082c511f62ba8
Daniel Frey <dmfrey@gmail.com>
2023-11-03 13:44:53 -0400

    add new node to cluster

--- infrastructure/talos/apply.sh
+++ infrastructure/talos/apply.sh
@@ -7,4 +7,5 @@ talosctl apply-config -n 192.168.30.23 -f ./clusterconfig/homelab-k8s-001-dmf-rp
 
 # talosctl apply-config -n 192.168.30.24 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-004.frey.dev.yaml
 talosctl apply-config -n 192.168.30.31 -f ./clusterconfig/homelab-k8s-001-dmf-amd-001.frey.dev.yaml
+talosctl apply-config -n 192.168.30.33 -f ./clusterconfig/homelab-k8s-001-dmf-amd-002.frey.dev.yaml
 talosctl apply-config -n 192.168.30.41 -f ./clusterconfig/homelab-k8s-001-dmf-nuc-001.frey.dev.yaml
