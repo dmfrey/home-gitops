d49288b90d0542174472d4eb15dd589299c6c87e
Daniel Frey <dmfrey@gmail.com>
2023-11-08 13:20:19 -0500

    add missing argument

--- terraform/media/sonarr/media-management.tf
+++ terraform/media/sonarr/media-management.tf
@@ -25,6 +25,7 @@ resource "sonarr_naming" "naming" {
   multi_episode_style        = 5
   colon_replacement_format   = 4
   daily_episode_format       = "{Series TitleYear} - {Air-Date} - {Episode CleanTitle} [{Custom Formats }{Quality Full}]{[MediaInfo VideoDynamicRangeType]}{[Mediainfo AudioCodec}{ Mediainfo AudioChannels]}{[MediaInfo VideoCodec]}{-Release Group}"
+  anime_episode_format       = "{Series TitleYear} - S{season:00}E{episode:00} - {absolute:000} - {Episode CleanTitle} [{Preferred Words }{Quality Full}]{[MediaInfo VideoDynamicRangeType]}[{MediaInfo VideoBitDepth}bit]{[MediaInfo VideoCodec]}[{Mediainfo AudioCodec} { Mediainfo AudioChannels}]{MediaInfo AudioLanguages}{-Release Group}"
   series_folder_format       = "{Series TitleYear} {imdb-{ImdbId}}"
   season_folder_format       = "Season {season:00}"
   specials_folder_format     = "Specials"
