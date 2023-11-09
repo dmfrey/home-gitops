ac841d0a7ec675b668a5961662a0a97c17a095fd
Daniel Frey <dmfrey@gmail.com>
2023-11-09 13:40:54 -0500

    update download client

--- terraform/downloads/prowlarr/download-client.tf
+++ terraform/downloads/prowlarr/download-client.tf
@@ -3,5 +3,7 @@ resource "prowlarr_download_client_qbittorrent" "rdt-client" {
   enable   = true
   host     = "rdt-client.download.svc.cluster.local"
   port     = 6500
+  username = "dmfrey"
+  password = "mbAxAYvNPgWBh99F$rs#"
   category = "misc"
 }
