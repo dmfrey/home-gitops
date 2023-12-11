f2f5a6b43d2d4043b95dd681d5ae20f6385285bc
Daniel Frey <dmfrey@gmail.com>
2023-12-11 14:50:14 -0500

    set reference to tag

--- terraform/downloads/prowlarr/indexer-proxy.tf
+++ terraform/downloads/prowlarr/indexer-proxy.tf
@@ -6,7 +6,5 @@ resource "prowlarr_indexer_proxy_flaresolverr" "flaresolverr" {
   host            = "http://flaresolverr.download.svc.cluster.local:8191"
   name            = "Flaresolverr"
   request_timeout = 60
-  tags = [
-    0
-  ]
+  tags            = [prowlarr_tag.flaresolverr.id]
 }
