d82bd242dd66832d771ff994b3abb6804acea6ce
Daniel Frey <dmfrey@gmail.com>
2023-12-13 12:57:29 -0500

    remove rpi nodes

--- infrastructure/talos/bootstrap.sh
+++ infrastructure/talos/bootstrap.sh
@@ -2,13 +2,14 @@
 
 echo "Applying Node Configs"
 # Deploy the configuration to the nodes
-talosctl apply-config --insecure -n 192.168.30.21 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-001.frey.dev.yaml
-talosctl apply-config --insecure -n 192.168.30.22 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-002.frey.dev.yaml
-talosctl apply-config --insecure -n 192.168.30.23 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-003.frey.dev.yaml
+# talosctl apply-config --insecure -n 192.168.30.21 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-001.frey.dev.yaml
+# talosctl apply-config --insecure -n 192.168.30.22 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-002.frey.dev.yaml
+# talosctl apply-config --insecure -n 192.168.30.23 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-003.frey.dev.yaml
 
 # talosctl apply-config --insecure -n 192.168.30.24 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-004.frey.dev.yaml
 talosctl apply-config --insecure -n 192.168.30.31 -f ./clusterconfig/homelab-k8s-001-dmf-amd-001.frey.dev.yaml
 talosctl apply-config --insecure -n 192.168.30.32 -f ./clusterconfig/homelab-k8s-001-dmf-amd-002.frey.dev.yaml
+talosctl apply-config --insecure -n 192.168.30.33 -f ./clusterconfig/homelab-k8s-001-dmf-amd-003.frey.dev.yaml
 talosctl apply-config --insecure -n 192.168.30.41 -f ./clusterconfig/homelab-k8s-001-dmf-nuc-001.frey.dev.yaml
 
 echo "Sleeping..."
