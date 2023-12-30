1331757124fdf726bfcd1e61bccd404f250c3b65
Daniel Frey <dmfrey@gmail.com>
2023-12-30 13:33:34 -0500

    remove remote path mappings

--- terraform/media/radarr/download-client.tf
+++ terraform/media/radarr/download-client.tf
@@ -3,14 +3,6 @@ resource "radarr_download_client_qbittorrent" "rdt-client" {
   enable                     = true
   host                       = "rdt-client.download.svc.cluster.local"
   port                       = 6500
-  username                   = "dmfrey"
-  password                   = "mbAxAYvNPgWBh99F$rs#"
   movie_category             = "movies"
   remove_completed_downloads = true
-}
-
-resource "radarr_remote_path_mapping" "downloads-path" {
-  host        = radarr_download_client_qbittorrent.rdt-client.host
-  remote_path = "/data/downloads/torrents/"
-  local_path  = "/media/downloads/torrents/"
 }
