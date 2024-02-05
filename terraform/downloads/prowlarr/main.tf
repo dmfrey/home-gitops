f5ed6e65c1fffa48ab93374275f023818bb41708
Daniel Frey <dmfrey@gmail.com>
2024-02-05 09:43:45 -0500

    update terraform

--- terraform/downloads/prowlarr/main.tf
+++ terraform/downloads/prowlarr/main.tf
@@ -1,18 +1,9 @@
 terraform {
+  required_version = ">= 1.3.9"
   required_providers {
     prowlarr = {
       source  = "devopsarr/prowlarr"
       version = "2.2.0"
     }
   }
-
-  cloud {
-    organization = "homelab5767"
-    workspaces {
-      name = "prowlarr-provisioner"
-    }
-  }
-
-  required_version = ">= 1.3.9"
-
 }
