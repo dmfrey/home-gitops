d66f2cb132a53d1a667215731873289da88dec7e
Daniel Frey <dmfrey@gmail.com>
2024-02-07 18:53:58 -0500

    add variable

--- terraform/media/radarr/variables.tf
+++ terraform/media/radarr/variables.tf
@@ -3,6 +3,11 @@ variable "RADARR_API_KEY" {
   sensitive = true
 }
 
+variable "SABNZBD_API_KEY" {
+  type      = string
+  sensitive = true
+}
+
 variable "radarr_url" {
   type    = string
   default = "http://radarr.media.svc.cluster.local:7878"
