5eaa063ff732a7f4c9bea27551ee48cdbf2e6850
Daniel Frey <dmfrey@gmail.com>
2024-03-01 09:56:42 -0500

    talos config updates

--- kubernetes/talos/apply.sh
+++ kubernetes/talos/apply.sh
@@ -6,6 +6,6 @@
 # talosctl apply-config -n 192.168.30.23 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-003.frey.dev.yaml
 
 # talosctl apply-config -n 192.168.30.24 -f ./clusterconfig/homelab-k8s-001-dmf-rpi-004.frey.dev.yaml
-talosctl apply-config -n 192.168.30.31 -f ./clusterconfig/homelab-k8s-001-dmf-amd-001.frey.dev.yaml
-talosctl apply-config -n 192.168.30.32 -f ./clusterconfig/homelab-k8s-001-dmf-amd-002.frey.dev.yaml
-talosctl apply-config -n 192.168.30.33 -f ./clusterconfig/homelab-k8s-001-dmf-amd-003.frey.dev.yaml
+talosctl apply-config -n 192.168.30.31 -f ./clusterconfig/homelab-k8s-001-dmf-amd-001.homelab.frey.dev.yaml
+talosctl apply-config -n 192.168.30.32 -f ./clusterconfig/homelab-k8s-001-dmf-amd-002.homelab.frey.dev.yaml
+talosctl apply-config -n 192.168.30.33 -f ./clusterconfig/homelab-k8s-001-dmf-amd-003.homelab.frey.dev.yaml
