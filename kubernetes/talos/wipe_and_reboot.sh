1dca0826e5b11c720d59ec094d0eb529d00d6c18
Daniel Frey <dmfrey@gmail.com>
2024-04-15 13:09:03 -0400

    updates to cluster

--- kubernetes/talos/wipe_and_reboot.sh
+++ kubernetes/talos/wipe_and_reboot.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 talosctl reset --system-labels-to-wipe STATE --system-labels-to-wipe EPHEMERAL --reboot -n 192.168.30.31,192.168.30.32,192.168.30.33
+
+
+talosctl reset --system-labels-to-wipe STATE --system-labels-to-wipe EPHEMERAL --user-disks-to-wipe /dev/nvme0n1 --wipe-mode user-disks --reboot -n 192.168.30.33
