5cff89b0b1a45efe9e54dad90a10f936b5d409d9
Daniel Frey <dmfrey@gmail.com>
2024-04-22 12:23:58 -0400

    update config

--- infrastructure/terraform/media/readarr/download-client.tf
+++ infrastructure/terraform/media/readarr/download-client.tf
@@ -1,21 +1,25 @@
 resource "readarr_download_client_qbittorrent" "rdt-client" {
   name                       = "rdt-client"
   enable                     = true
+  priortity                  = 25
   host                       = "rdt-client.download.svc.cluster.local"
   port                       = 6500
-  book_category             = "book"
+  book_category              = "book"
   remove_completed_downloads = true
+  first_and_last             = true
+  tags                       = ["book"]
 }
 
 resource "readarr_download_client_sabnzbd" "sabnzbd" {
-  enable   = true
-  priority = 1
-  name     = "sabnzbd"
-  host     = "sabnzbd.download.svc.cluster.local"
-  url_base = "/sabnzbd/"
-  port     = 8080
-  api_key  = var.SABNZBD_API_KEY
-  book_category = "book"
+  name                       = "sabnzbd"
+  enable                     = true
+  priority                   = 1
+  host                       = "sabnzbd.download.svc.cluster.local"
+  port                       = 8080
+  url_base                   = "/sabnzbd/"
+  api_key                    = var.SABNZBD_API_KEY
+  book_category              = "book"
+  tags                       = ["book"]
 }
 
 resource "readarr_remote_path_mapping" "sabnzbd" {
