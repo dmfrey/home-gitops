4a6c5f49e58f9c8ceff801e024fd1d432eee7e86
Daniel Frey <dmfrey@gmail.com>
2024-09-03 15:43:31 -0400

    update config

---
+++ infrastructure/terraform/security/authentik/scopes.tf
@@ -0,0 +1,8 @@
+data "authentik_scope_mapping" "scopes"  {
+  managed_list = [
+    "goauthentik.io/providers/oauth2/scope-email",
+    "goauthentik.io/providers/oauth2/scope-openid",
+    "goauthentik.io/providers/oauth2/scope-profile"
+  ]
+}
+
