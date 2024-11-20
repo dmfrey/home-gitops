9c5774496357fe6094fba4241d9ef1425b026984
Daniel Frey <dmfrey@gmail.com>
2024-11-20 14:33:31 -0500

    update secret keys

--- infrastructure/terraform/security/authentik/applications.tf
+++ infrastructure/terraform/security/authentik/applications.tf
@@ -1,7 +1,7 @@
 locals {
   oauth_apps = [
     "grafana",
-    "spring_dev"
+    "spring-dev"
   ]
 }
 
@@ -20,8 +20,8 @@ locals {
   parsed_secrets = {
     for app, secret in data.bitwarden_secret.application : app => {
       raw_data            = jsondecode(secret.value)
-      client_id           = local.raw_data["${upper(app)}_CLIENT_ID"]
-      client_secret       = local.raw_data["${upper(app)}_CLIENT_SECRET"]
+      client_id           = local.raw_data["AUTHENTIK_CLIENT_ID"]
+      client_secret       = local.raw_data["AUTHENTIK_CLIENT_SECRET"]
     }
   }
 }
