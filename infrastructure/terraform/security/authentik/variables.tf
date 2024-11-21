1c3de3339857f1a319588457b069052532a444f8
Daniel Frey <dmfrey@gmail.com>
2024-11-21 13:43:22 -0500

    update user object

--- infrastructure/terraform/security/authentik/variables.tf
+++ infrastructure/terraform/security/authentik/variables.tf
@@ -21,13 +21,8 @@ variable "users" {
   type = map(object({
     name   = string
     email  = string
-    password = var.user_password
+    password = string
     groups = list(string)
   }))
-}
-
-variable "user_password" {
-  description = "User's password"
-  type = string
   sensitive = true
 }
