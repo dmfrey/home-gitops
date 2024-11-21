d38b2493e75f49ce9b5e4785aba478f575e4a4cf
Daniel Frey <dmfrey@gmail.com>
2024-11-21 15:19:40 -0500

    update users

--- infrastructure/terraform/security/authentik/users.tf
+++ infrastructure/terraform/security/authentik/users.tf
@@ -1,11 +1,15 @@
 resource "authentik_user" "users" {
   for_each       = var.users
-  name           = each.value["name"]
   username       = each.key
+  name           = each.value["name"]
   email          = each.value["email"]
   password       = each.value["password"]
   groups         = [
     for desired_group in each.value.groups :
     data.authentik_group.lookup_by_name[desired_group].id
   ]
+}
+
+output "var_users" {
+  value = var.users
 }
