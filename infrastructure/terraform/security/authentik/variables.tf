cfc779305f929bfa35991bb169f95bc849ae9559
Daniel Frey <dmfrey@gmail.com>
2024-11-21 10:55:47 -0500

    add users

--- infrastructure/terraform/security/authentik/variables.tf
+++ infrastructure/terraform/security/authentik/variables.tf
@@ -1,38 +1,3 @@
-# variable "authentik_url" {
-#   type        = string
-#   description = "authentik url used to connect"
-#   default     = "http://authentik-server.security.svc.cluster.local:80"
-# }
-
-# variable "AUTHENTIK_BOOTSTRAP_TOKEN" {
-#   type        = string
-#   description = "authentik token used for authentication to api"
-#   sensitive   = true
-# }
-
-# variable "AUTHENTIK_PLEX_CLIENT_ID" {
-#   type        = string
-#   description = "client id to use for plex source authentication"
-#   sensitive   = true
-# }
-
-# variable "AUTHENTIK_PLEX_TOKEN" {
-#   type        = string
-#   description = "token to use for plex source authentication"
-#   sensitive   = true
-# }
-
-# variable "PLEX_SERVER_ID" {
-#   type        = string
-#   description = "ID of authorized Plex server"
-#   sensitive   = true
-# }
-
-# variable "authentik_host" {
-#   type        = string
-#   description = "public url for authentik"
-# }
-
 # variable "proxy_applications" {
 #   type = map(object({
 #     url             = string
@@ -40,38 +5,6 @@
 #     skip_path_regex = optional(string)
 #   }))
 # }
-
-# variable "oauth2_applications" {
-#   type = map(object({
-#     url           = string
-#     group         = string
-#     client_id     = string
-#     client_secret = string
-#     redirect_uris = list(string)
-#   }))
-# }
-
-# variable "groups" {
-#   type = map(object({
-#     name      = string
-#     parent    = optional(string)
-#     superuser = optional(bool)
-#   }))
-# }
-
-# variable "users" {
-#   type = map(object({
-#     name   = string
-#     email  = string
-#     # groups = list(string)
-#   }))
-# }
-
-# variable "external_domain" {
-#   type      = string
-#   sensitive = true
-# }
-
 
 variable "cluster_domain" {
   type      = string
@@ -82,4 +15,12 @@ variable "bw_access_token" {
   type        = string
   description = "Bitwarden Secret Manager Access token"
   sensitive   = true
+}
+
+variable "users" {
+  type = map(object({
+    name   = string
+    email  = string
+    groups = list(string)
+  }))
 }
