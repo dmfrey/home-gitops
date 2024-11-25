36cef839ea20923d10d5d52841bbd546800065f2
Daniel Frey <dmfrey@gmail.com>
2024-11-24 20:40:07 -0500

    change user

--- infrastructure/terraform/security/authentik/tokens.tf
+++ infrastructure/terraform/security/authentik/tokens.tf
@@ -1,11 +1,7 @@
 
 resource "authentik_token" "homepage" {
   identifier = "homepage"
-  user      = authentik_user.dmfrey.id
+  user      = data.authentik_user.akadmin.id
   expiring  = false
-  intent    = api
-
-  depends_on = [
-    authentik_user.dmfrey
-  ]
+  intent    = "api"
 }
