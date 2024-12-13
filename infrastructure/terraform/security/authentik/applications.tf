138a2ad82da66ee6fac833f9d489b5bbf7426303
Daniel Frey <dmfrey@gmail.com>
2024-12-12 20:17:18 -0500

    remove comment

--- infrastructure/terraform/security/authentik/applications.tf
+++ infrastructure/terraform/security/authentik/applications.tf
@@ -10,7 +10,6 @@ resource "authentik_provider_oauth2" "oauth2" {
   property_mappings     = data.authentik_property_mapping_provider_scope.oauth2.ids
   access_token_validity = "hours=4"
   signing_key           = data.authentik_certificate_key_pair.generated.id
-  # redirect_uris         = [each.value.redirect_uri]
   allowed_redirect_uris = [
     {
       matching_mode = "strict",
