b66515c006333aba0c4e4830a4fa96f6a1966e9a
Daniel Frey <dmfrey@gmail.com>
2024-12-31 21:16:38 -0500

    add new node

--- kubernetes/homelab-k8s-001/talos/apply.sh
+++ kubernetes/homelab-k8s-001/talos/apply.sh
@@ -5,3 +5,4 @@ talosctl apply-config -n 192.168.30.31 -f ./clusterconfig/homelab-k8s-001-dmf-am
 talosctl apply-config -n 192.168.30.32 -f ./clusterconfig/homelab-k8s-001-dmf-amd-002.yaml --dry-run
 talosctl apply-config -n 192.168.30.33 -f ./clusterconfig/homelab-k8s-001-dmf-amd-003.yaml --dry-run
 talosctl apply-config -n 192.168.30.41 -f ./clusterconfig/homelab-k8s-001-dmf-intel-001.yaml
+talosctl apply-config -n 192.168.30.42 -f ./clusterconfig/homelab-k8s-001-dmf-intel-002.yaml
--- kubernetes/homelab-k8s-001/talos/bootstrap.sh
+++ kubernetes/homelab-k8s-001/talos/bootstrap.sh
@@ -7,6 +7,7 @@ talosctl apply-config --insecure -n 192.168.30.32 -f ./clusterconfig/homelab-k8s
 talosctl apply-config --insecure -n 192.168.30.33 -f ./clusterconfig/homelab-k8s-001-dmf-amd-003.yaml
 
 talosctl apply-config --insecure -n 192.168.30.41 -f ./clusterconfig/homelab-k8s-001-dmf-intel-001.yaml
+talosctl apply-config --insecure -n 192.168.30.42 -f ./clusterconfig/homelab-k8s-001-dmf-intel-002.yaml
 
 echo "Sleeping..."
 sleep 120
