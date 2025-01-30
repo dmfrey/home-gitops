7ba4926340e078ac371672089028c195afb592fb
Daniel Frey <dmfrey@gmail.com>
2025-01-30 14:14:36 -0500

    update users

--- infrastructure/terraform/security/authentik/users.tf
+++ infrastructure/terraform/security/authentik/users.tf
@@ -5,20 +5,20 @@ resource "authentik_user" "users" {
   name           = each.value["name"]
   email          = each.value["email"]
   password       = each.value["password"]
-  groups         = [
-    for desired_group in each.value["groups"] :
-    data.authentik_group.lookup_by_name[desired_group].id
-  ]
+  # groups         = [
+  #   for desired_group in each.value["groups"] :
+  #   data.authentik_group.lookup_by_name[desired_group].id
+  # ]
 
-  depends_on = [
-    authentik_group.developers,
-    authentik_group.infrastructure,
-    authentik_group.grafana_admin,
-    authentik_group.monitoring,
-    authentik_group.users,
-    authentik_group.downloads,
-    authentik_group.home
-  ]
+  # depends_on = [
+  #   authentik_group.developers,
+  #   authentik_group.infrastructure,
+  #   authentik_group.grafana_admin,
+  #   authentik_group.monitoring,
+  #   authentik_group.users,
+  #   authentik_group.downloads,
+  #   authentik_group.home
+  # ]
 }
 
 data "authentik_user" "akadmin" {
