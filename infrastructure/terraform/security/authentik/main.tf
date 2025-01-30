ad6e78016962509b9c0ec2ff9726e07376064ce3
Daniel Frey <dmfrey@gmail.com>
2025-01-30 13:09:53 -0500

    add 1password provider

--- infrastructure/terraform/security/authentik/main.tf
+++ infrastructure/terraform/security/authentik/main.tf
@@ -6,29 +6,26 @@ terraform {
       version = "2024.12.0"
     }
 
-    # onepassword = {
-    #   source  = "1Password/onepassword"
-    #   version = "2.1.2"
-    # }
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "2.1.2"
+    }
 
   }
 
 }
 
-# data "onepassword_vault" "vault" {
-#   name = var.vault
-# }
+provider "onepassword" {
+  url                   = var.service_account_json != null ? "http://onepassword-connect.external-secrets.svc.cluster.local" : null
+  token                 = var.service_account_json
+  service_account_token = var.onepassword_sa_token
+}
 
-# data "onepassword_item" "item" {
-#   vault = data.onepassword_vault.vault.uuid
-#   title = var.item
-# }
-
-# provider "onepassword" {
-#   url                   = var.service_account_json != null ? "http://onepassword-connect.external-secrets.svc.cluster.local" : null
-#   token                 = var.service_account_json
-#   service_account_token = var.onepassword_sa_token
-# }
+module "onepassword_authentik" {
+  source = "github.com/dmfrey/terraform-1password-item"
+  vault  = "homelab5767"
+  item   = "authentik"
+}
 
 locals {
   authentik_token             = local.raw_data["AUTHENTIK_TOKEN"]
@@ -38,5 +35,5 @@ locals {
 
 provider "authentik" {
   url   = "https://auth.${var.cluster_domain}"
-  token = local.raw_data["AUTHENTIK_TOKEN"]
+  token = module.onepassword_authentik.fields["AUTHENTIK_TOKEN"]
 }
--- infrastructure/terraform/security/authentik/variables.tf
+++ infrastructure/terraform/security/authentik/variables.tf
@@ -1,29 +1,17 @@
 
-# variable "onepassword_sa_token" {
-#   type        = string
-#   description = "Oneopass Service Account Token"
-#   sensitive   = true
-#   default     = null
-# }
+variable "onepassword_sa_token" {
+  type        = string
+  description = "Oneopass Service Account Token"
+  sensitive   = true
+  default     = null
+}
 
-# variable "service_account_json" {
-#   type        = string
-#   description = "The path to the service account JSON for OnePassword."
-#   sensitive   = true
-#   default     = null
-# }
-
-# variable "vault" {
-#   description = "Vault name to find the Item in"
-#   type        = string
-#   default     = "homelab5767"
-# }
-
-# variable "item" {
-#   description = "Password Item to fetch"
-#   type        = string
-#   default     = "authentik"
-# }
+variable "service_account_json" {
+  type        = string
+  description = "The path to the service account JSON for OnePassword."
+  sensitive   = true
+  default     = null
+}
 
 variable "cluster_domain" {
   type      = string
