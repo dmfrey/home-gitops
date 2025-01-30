711cb228fb866383ad57b02ec833a6294fd6fa91
Daniel Frey <dmfrey@gmail.com>
2025-01-30 13:54:03 -0500

    update directory

--- infrastructure/terraform/security/authentik/directory.tf
+++ infrastructure/terraform/security/authentik/directory.tf
@@ -1,16 +1,18 @@
 
+locals {
+  authentik_groups = {
+    developers     = { name = "Developers" }
+    downloads      = { name = "Downloads" }
+    home           = { name = "Home" }
+    infrastructure = { name = "Infrastructure" }
+    monitoring     = { name = "Monitoring" }
+    media          = { name = "Media" }
+    users          = { name = "Users" }
+  }
+}
+
 data "authentik_group" "admins" {
   name = "authentik Admins"
-}
-
-resource "authentik_group" "developers" {
-  name         = "Developers"
-  is_superuser = false
-}
-
-resource "authentik_group" "infrastructure" {
-  name         = "Infrastructure"
-  is_superuser = false
 }
 
 resource "authentik_group" "grafana_admin" {
@@ -18,56 +20,78 @@ resource "authentik_group" "grafana_admin" {
   is_superuser = false
 }
 
-resource "authentik_group" "monitoring" {
-  name         = "Monitoring"
-  is_superuser = false
-  parent       = resource.authentik_group.grafana_admin.id
-}
-
-resource "authentik_group" "users" {
-  name         = "Users"
+resource "authentik_group" "default" {
+  for_each     = local.authentik_groups
+  name         = each.value.name
   is_superuser = false
 }
 
-resource "authentik_group" "downloads" {
-  name         = "Downloads"
-  is_superuser = false
-  parent       = resource.authentik_group.users.id
-}
 
-resource "authentik_group" "home" {
-  name         = "Home"
-  is_superuser = false
-  parent       = resource.authentik_group.users.id
-}
+# resource "authentik_group" "developers" {
+#   name         = "Developers"
+#   is_superuser = false
+# }
 
-resource "authentik_group" "media" {
-  name         = "Media"
-  is_superuser = false
-  parent       = resource.authentik_group.users.id
-}
+# resource "authentik_group" "infrastructure" {
+#   name         = "Infrastructure"
+#   is_superuser = false
+# }
 
-data "authentik_groups" "all" {
+# resource "authentik_group" "grafana_admin" {
+#   name         = "Grafana Admins"
+#   is_superuser = false
+# }
 
-}
+# resource "authentik_group" "monitoring" {
+#   name         = "Monitoring"
+#   is_superuser = false
+#   parent       = resource.authentik_group.grafana_admin.id
+# }
+
+# resource "authentik_group" "users" {
+#   name         = "Users"
+#   is_superuser = false
+# }
+
+# resource "authentik_group" "downloads" {
+#   name         = "Downloads"
+#   is_superuser = false
+#   parent       = resource.authentik_group.users.id
+# }
+
+# resource "authentik_group" "home" {
+#   name         = "Home"
+#   is_superuser = false
+#   parent       = resource.authentik_group.users.id
+# }
+
+# resource "authentik_group" "media" {
+#   name         = "Media"
+#   is_superuser = false
+#   parent       = resource.authentik_group.users.id
+# }
+
+# data "authentik_groups" "all" {
+
+# }
 
 output all_groups{
   value = data.authentik_groups.all.groups
   description = "The registered authentik groups"
 }
 
-data "authentik_group" "lookup_by_application" {
-  for_each = var.oauth_applications
-  name     = each.value.group
-}
+# data "authentik_group" "lookup_by_application" {
+#   for_each = var.oauth_applications
+#   name     = each.value.group
+# }
 
-data "authentik_group" "lookup_by_name" {
-  for_each = {
-    for group in data.authentik_groups.all.groups:
-      group.name => group
-  }
-  name = each.value.name
-}
+# data "authentik_group" "lookup_by_name" {
+#   for_each = {
+#     for group in data.authentik_groups.all.groups:
+#       group.name => group
+#   }
+#   name = each.value.name
+# }
 
 resource "authentik_policy_binding" "application_policy_binding" {
   for_each = var.oauth_applications
