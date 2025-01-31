853990cf8bdc7090db907b6b2caa749129da85f3
Daniel Frey <dmfrey@gmail.com>
2025-01-30 20:09:10 -0500

    fix tokens

--- infrastructure/terraform/security/authentik/tokens.tf
+++ infrastructure/terraform/security/authentik/tokens.tf
@@ -1,11 +1,11 @@
 
-resource "authentik_user" "dmfrey" {
+data "authentik_user" "dmfrey" {
   username = "dmfrey"
 }
 
 resource "authentik_token" "homepage" {
   identifier  = "homepage-token"
-  user        = authentik_user.dmfrey.id
+  user        = data.authentik_user.dmfrey.id
   description = "Homepage access token"
   expiring    = false
 }
