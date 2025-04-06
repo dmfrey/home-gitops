1e09d12fa32e435d4a772570f12ef8daa4a80125
Daniel Frey <dmfrey@gmail.com>
2025-04-06 08:16:41 -0400

    remove old code

--- infrastructure/terraform/storage/volsync/bucket.tf
+++
@@ -1,4 +0,0 @@
-resource "b2_bucket" "volsync" {
-  bucket_name = var.B2_BUCKET_NAME
-  bucket_type = "allPrivate"
-}
--- infrastructure/terraform/storage/volsync/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "b2" {
-  application_key    = var.B2_APPLICATION_KEY
-  application_key_id = var.B2_APPLICATION_KEY_ID
-}
