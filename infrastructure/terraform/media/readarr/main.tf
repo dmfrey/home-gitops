c110a673bbfb6111a193a924fddbec54f4ca2ea4
Daniel Frey <dmfrey@gmail.com>
2026-03-06 09:30:31 -0500

    remove unused terraform

--- infrastructure/terraform/media/readarr/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    readarr = {
-      source  = "devopsarr/readarr"
-      version = "2.1.0"
-    }
-  }
-}
--- infrastructure/terraform/media/readarr/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "readarr" {
-  url     = var.readarr_url
-  api_key = var.READARR_API_KEY
-}
